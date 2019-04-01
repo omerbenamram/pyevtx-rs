78a7cb4d7334a11e21da65cebda5e3cd85c86224
Omer Ben-Amram <omerbenamram@gmail.com>
2019-04-01 14:14:35 +0300

    add another test

--- tests/test_pyevtx.py
+++ tests/test_pyevtx.py
@@ -1,6 +1,15 @@
+import pytest
 from pyevtx_rs.evtx_parser import PyEvtxParser
+
 
 def test_it_works():
     parser = PyEvtxParser("/Users/omerba/Workspace/evtx-rs/samples/security.evtx")
     for record in parser:
         print(record)
+
+
+def test_it_returns_error_on_non_existing_path():
+    with pytest.raises(RuntimeError):
+        parser = PyEvtxParser("non_existing")
+
+
