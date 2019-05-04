fb9142f7ac9e5a63cc9c0ee8176c454f6dc442f0
Omer Ben-Amram <omerbenamram@gmail.com>
2019-05-04 23:25:22 +0300

    0.2.7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = ["setuptools-rust"]
 
 setup(
     name="evtx",
-    version="0.2.6",
+    version="0.2.7",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Development Status :: 3 - Alpha",
