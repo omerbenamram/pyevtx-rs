7697b678e156f634c09b5adc42f6810f1c605df2
Omer Ben-Amram <omerbenamram@gmail.com>
2022-10-01 22:45:55 +0300

    update README.md
    use ${{ env.GITHUB_WORKSPACE }}
    
    use PWD
    
    handle exception in `evtx_dump.py`
    
    use correct dir

--- scripts/evtx_dump.py
+++ scripts/evtx_dump.py
@@ -7,11 +7,15 @@ def main():
     # first parameter is the path to the evtx file
     evtx_file = os.path.abspath(os.path.expanduser(sys.argv[1]))
     parser = PyEvtxParser(evtx_file, number_of_threads=0)
-    for record in parser.records():
-        print(f'Event Record ID: {record["event_record_id"]}')
-        print(f'Event Timestamp: {record["timestamp"]}')
-        print(record['data'])
-        print(f'------------------------------------------')
+    try:
+        for record in parser.records():
+            print(f'Event Record ID: {record["event_record_id"]}')
+            print(f'Event Timestamp: {record["timestamp"]}')
+            print(record['data'])
+            print(f'------------------------------------------')
+    except RuntimeError as e:
+        print(f'Error: {e}')
+        exit(0)
 
 if __name__ == '__main__':
     main()
